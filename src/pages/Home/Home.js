9963a46451f976d818371bd799db12a0c556071b
Gagan Singh <gagansingh.dev@outlook.com>
2020-09-02 21:50:08 -0400

    Added recommended cities fetching logic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./Home.css";
 import SearchBox from "./SearchBox";
+import Spinner from "../../utility/Spinner/Spinner";
 
 class Home extends Component {
+    state = {
+        cities: [],
+    };
+
+    async componentDidMount() {
+        const recommendedCities = await axios.get(
+            `${process.env.REACT_APP_API_URL}/cities/recommended`
+        );
+        this.setState({ cities: recommendedCities.data });
+    }
+
     render() {
+        if (this.state.cities.length === 0) {
+            return <Spinner />;
+        }
         return (
             <div className="container-fluid">
                 <div className="row">
---
+++ src/utility/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./Spinner.css";
+
+class Spinner extends Component {
+    render() {
+        return <div> Spinner</div>;
+    }
+}
+
+export default Spinner;
